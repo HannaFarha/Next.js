b3aa9e1b7783c427c336fd27afacecf82a849055
HannaFarha <johnnyfarha1@gmail.com>
2024-03-10 14:10:14 +0100

    page.js

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,5 +1,15 @@
-export default function PostPage ()  {
-    return ( <div><h1>Post Page</h1></div> );
+export default async function PostPage ()  {
+    const response = await fetch ("https://jsonplaceholder.typicode.com/posts",{
+next: {
+    revalidate: 120 ,},
+
+    });
+    const posts =await response.json();
+    const postsJSX =posts.map((post)=>{
+
+        return ( <div><h1>{post.title}</h1></div> );
+})
+    
+    
 }
- 
  
